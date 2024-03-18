be4e6b8071dcbdd5e7338a973d5a9c6a29d34457
Arthur Maestri <40868046+ArthurMaes@users.noreply.github.com>
2024-03-17 22:00:15 -0300

    Update and rename BubblueSort.java to BubbleSort.java

--- ArthurMaestri_RA4231925821/Atividade3/BubbleSort.java
+++ ArthurMaestri_RA4231925821/Atividade3/BubbleSort.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class BubblueSort {   
+public class BubbleSort {   
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
